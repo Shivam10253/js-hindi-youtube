e3bf5c543b75b447149d4f1f81ec20aa3c5070e5
Shivam <148491558+Shivam10253@users.noreply.github.com>
2026-03-02 16:40:16 +0000

    #

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+
+const accountID = 10253
+let accountEmail = "shivamsingh10253@gmail.com"
+var password = "123"
+accountCity = "Delhi"
+
+// accountID = 2 // cannot change because we assign the value in const type
+// console.log(accountID);
+
+console.table([accountEmail, password, accountCity])
+
+// Here, we can change the value for let and var types of variables
+accountEmail = "s10253@gmail.com"
+password =  "1234"
+accountCity = "Mumbai"
+
+console.table([accountEmail, password, accountCity])
