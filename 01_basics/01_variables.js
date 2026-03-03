65a281b3f604ddd6eff130cd90d281e7b7c7fd2e
Shivam <148491558+Shivam10253@users.noreply.github.com>
2026-03-03 06:50:31 +0000

    learn about variables in js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,17 +1,25 @@
 
-const accountID = 10253
-let accountEmail = "shivamsingh10253@gmail.com"
-var password = "123"
-accountCity = "Delhi"
+const accountID = 10253;
+let accountEmail = "shivamsingh10253@gmail.com";
+var password = "123";
+
+/*
+    Prefer not to use var
+    because of issue in block scope and functional scope
+*/
+
+accountCity = "Delhi";
+let accountState;
 
 // accountID = 2 // cannot change because we assign the value in const type
 // console.log(accountID);
 
-console.table([accountEmail, password, accountCity])
+// console.table([accountEmail, password, accountCity]);
 
 // Here, we can change the value for let and var types of variables
-accountEmail = "s10253@gmail.com"
-password =  "1234"
-accountCity = "Mumbai"
+accountEmail = "s10253@gmail.com";
+password =  "1234";
+accountCity = "Mumbai";
 
-console.table([accountEmail, password, accountCity])
+console.table([accountEmail, password, accountCity, accountState]);
+
